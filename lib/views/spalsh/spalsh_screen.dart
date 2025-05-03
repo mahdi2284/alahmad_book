4a2cb254892ac12c9cbf09ab84eb19e684bc4be0
mahdi abbasi <mahdi.abbasi2284@hotmail.com>
2025-05-03 13:34:00 +0330

    new

---
+++ lib/views/spalsh/spalsh_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class SpalshScreen extends StatefulWidget {
+  const SpalshScreen({super.key});
+
+  @override
+  State<SpalshScreen> createState() => _SpalshScreenState();
+}
+
+class _SpalshScreenState extends State<SpalshScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        decoration: BoxDecoration( ),
+      ),
+    );
+  }
+}
